2f56223b37eb2c5d9ecb110c7f2e5b55db5e86c0
cntbabul <cntbabul@gmail.com>
2026-03-11 10:48:16 +0530

    docker rm and frontend backend separated

---
+++ backend/src/test-path.ts
@@ -0,0 +1,3 @@
+import path from "path";
+console.log(path.join(__dirname, "../../web/dist"));
+console.log(path.resolve(__dirname, "../../web/dist"));
---
+++ backend/test-path-root.ts
@@ -0,0 +1,3 @@
+import path from "path";
+console.log(path.join(process.cwd(), "../web/dist"));
+console.log(path.resolve(process.cwd(), "../web/dist"));
